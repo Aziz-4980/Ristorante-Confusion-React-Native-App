3c941e9fa742ae3f2f66d963418222c9f9200059
Abdul Aziz Khan <abdul.aziz.khan.1122@gmail.com>
2020-05-23 00:47:19 +0500

    Gestures part 1

--- components/DishdetailComponent.js
+++ components/DishdetailComponent.js
@@ -1,13 +1,6 @@
 import React, { Component } from "react";
-import {
-  Text,
-  View,
-  ScrollView,
-  FlatList,
-  Modal,
-  StyleSheet,
-  Button
-} from "react-native";
+import { Text, View, ScrollView, FlatList, Modal, StyleSheet, Button, Alert, PanResponder } from 'react-native';
+
 import { Card, Icon, Input } from "react-native-elements";
 import { connect } from "react-redux";
 import { baseUrl } from "../shared/baseUrl";
@@ -29,13 +22,42 @@ const mapDispatchToProps = dispatch => ({
     dispatch(postComment(dishId, rating, author, comment))
 });
 
+
 function RenderDish(props) {
   const dish = props.dish;
+  const recognizeDrag = ({ moveX, moveY, dx, dy }) => {
+    if ( dx < -200 )
+        return true;
+    else
+        return false;
+}
 
-  if (dish != null) {
-    return (
-      <Animatable.View animation="fadeInDown" duration={2000} delay={1000}>
-      <Card
+const panResponder = PanResponder.create({
+    onStartShouldSetPanResponder: (e, gestureState) => {
+        return true;
+    },
+    onPanResponderEnd: (e, gestureState) => {
+        console.log("pan responder end", gestureState);
+        if (recognizeDrag(gestureState))
+            Alert.alert(
+                'Add Favorite',
+                'Are you sure you wish to add ' + dish.name + ' to favorite?',
+                [
+                {text: 'Cancel', onPress: () => console.log('Cancel Pressed'), style: 'cancel'},
+                {text: 'OK', onPress: () => {props.favorite ? console.log('Already favorite') : props.onPressFavorite()}},
+                ],
+                { cancelable: false }
+            )
+
+        return true;
+    }
+}); 
+
+if (dish != null) {
+    return(
+        <Animatable.View animation="fadeInDown" duration={2000} delay={1000}
+        {...panResponder.panHandlers}>
+ <Card
       featuredTitle={dish.name}
       image={{uri: baseUrl + dish.image}}>
   <Text style={{ margin: 10 }}>{dish.description}</Text>
