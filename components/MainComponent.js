873b97fa5f764192d4c07acc21c537e0b7f8206c
Abdul Aziz Khan <abdul.aziz.khan.1122@gmail.com>
2020-05-24 03:53:32 +0500

    Network Info

--- components/MainComponent.js
+++ components/MainComponent.js
@@ -6,7 +6,7 @@ import ContactUs from './ContactComponent';
 // import { View,Platform } from 'react-native';
 // import { createStackNavigator, createDrawerNavigator } from 'react-navigation';
 import AboutUs from './AboutComponent';
-import { View, Platform, Text, ScrollView, Image, StyleSheet } from 'react-native';
+import { View, Platform, Text, ScrollView, Image, StyleSheet, ToastAndroid } from 'react-native';
 import { createStackNavigator, createDrawerNavigator, DrawerItems, SafeAreaView } from 'react-navigation';
 import { Icon } from 'react-native-elements';
 import Constants from 'expo-constants';
@@ -15,6 +15,7 @@ import { fetchDishes, fetchComments, fetchPromos, fetchLeaders } from '../redux/
 import Reservation from './ReservationComponent';
 import Favorites from './FavoriteComponent';
 import Login from './LoginComponent';
+import NetInfo from '@react-native-community/netinfo';
 
 const mapStateToProps = state => {
   return {
@@ -325,15 +326,63 @@ const MainNavigator = createDrawerNavigator({
 });
 
 
-class Main extends Component { 
-    
+class Main extends Component {
 
   componentDidMount() {
     this.props.fetchDishes();
     this.props.fetchComments();
     this.props.fetchPromos();
     this.props.fetchLeaders();
-  }
+
+
+    
+    // NetInfo.getConnectionInfo()
+    //     .then((connectionInfo) => {
+    //         ToastAndroid.show('Initial Network Connectivity Type: '
+    //             + connectionInfo.type + ', effectiveType: ' + connectionInfo.effectiveType,
+    //             ToastAndroid.LONG)
+    //     });
+
+  //       NetInfo.fetch().then(state => {
+  //         console.log("Initial Network Connectivity Type: "
+  //         + connectionInfo.type + ', effectiveType: ' + connectionInfo.effectiveType,
+  //         ToastAndroid.LONG, state.type);
+
+  //         console.log("Is connected?", state.isConnected);
+  //       });
+
+  //   NetInfo.addEventListener('connectionChange', this.handleConnectivityChange);
+  // }
+
+  // componentWillUnmount() {
+  //   NetInfo.removeEventListener('connectionChange', this.handleConnectivityChange);
+  // }
+
+  // handleConnectivityChange = (connectionInfo) => {
+  //   switch (connectionInfo.type) {
+  //     case 'none':
+  //       ToastAndroid.show('You are now offline!', ToastAndroid.LONG);
+  //       break;
+  //     case 'wifi':
+  //       ToastAndroid.show('You are now connected to WiFi!', ToastAndroid.LONG);
+  //       break;
+  //     case 'cellular':
+  //       ToastAndroid.show('You are now connected to Cellular!', ToastAndroid.LONG);
+  //       break;
+  //     case 'unknown':
+  //       ToastAndroid.show('You now have unknown connection!', ToastAndroid.LONG);
+  //       break;
+  //     default:
+  //       break;
+  //   }
+  // }
+  const unsubscribe = NetInfo.addEventListener(state => {
+    console.log("Connection type", state.type);
+    console.log("Is connected?", state.isConnected);
+  });
+  unsubscribe();
+}
+
     render(){
         return(
             <View style={{ flex: 1, paddingTop: Platform.OS ==='ios' ? 0 : Constants.statusBarHeight }}>
